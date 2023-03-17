800a2e46d668fbae9d2e031a435d03415a5f141d
mohamed <malaga.142014@gmail.com>
2023-03-17 15:42:38 -0700

    done

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - print one line
+ *
+ * Return: does not return
+ */
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
