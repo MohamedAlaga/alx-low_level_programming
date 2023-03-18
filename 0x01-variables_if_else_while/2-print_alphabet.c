3685334826473678c2f696dfa7dea1d614a6da57
mohamed <malaga.142014@gmail.com>
2023-03-17 19:26:57 -0700

    .

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
-char alpha [27] ={'a' ,'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u' ,'v' ,'w' ,'x' ,'y' ,'z' ,"\n"};
+char alpha [26] ={'a' ,'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t' ,'u' ,'v' ,'w' ,'x' ,'y' ,'z' };
 int i;
 
-for (i = 0 ;i<27 ;i ++)
+for (i = 0 ;i<26 ;i ++)
 {
 putchar(alpha[i]);
 }
+putchar('\n');
 return (0);
 }
