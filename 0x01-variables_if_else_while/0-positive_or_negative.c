afd8d107211f6ba96cd8055c37a195c95d543f45
mohamed <malaga.142014@gmail.com>
2023-03-17 18:04:11 -0700

    done

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/**
+ * main - chose a random number
+ *
+ * descreption:giving a rndom number and print if positve or nigatne
+ * Return: just end the program
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{printf("is positive"); }
+return (0);
+if (n < 0)
+{printf("is negative"); }
+else
+{printf("is zero"); }
+return (0);
+}
