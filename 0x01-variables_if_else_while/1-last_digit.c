a1363f7bea0c7cc11693003fef4cc05cd1c61918
mohamed <malaga.142014@gmail.com>
2023-03-17 18:51:35 -0700

    d

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,15 +19,15 @@ y = y * 10;
 y = n - y;
 if (y > 5)
 {
-printf("Last digit of %d is %d and is greater than 5", n, y);
+printf("Last digit of %d is %d and is greater than 5\n", n, y);
 }
 else if (y == 0)
 {
-printf("Last digit of %d is %d and is 0", n, y);
+printf("Last digit of %d is %d and is 0\n", n, y);
 }
 else
 {
-printf("Last digit of %d is %d and is less than 6 and not 0", n, y);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, y);
 }
 return (0);
 }
