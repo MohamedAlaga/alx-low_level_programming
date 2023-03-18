5c68dca87bee00c6e5ce32066d4cdb3fcacede8d
mohamed <malaga.142014@gmail.com>
2023-03-17 19:39:15 -0700

    d

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,12 +8,11 @@
 
 int main(void)
 {
-char alpha [26] = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-int i;
+char alpha ;
 
-for (i = 0; i < 26; i++)
+for (alpha = "a"; alpha = "z"; alpha++)
 {
-putchar(alpha[i]);
+putchar(alpha);
 }
 putchar('\n');
 return (0);
