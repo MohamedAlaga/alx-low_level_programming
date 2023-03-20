6e62fa68cac86655cde47a0d32f143d4189cee06
mohamed <malaga.142014@gmail.com>
2023-03-20 15:28:06 -0700

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,13 +9,12 @@ int main(void)
 int i;
 for (i = 0 ; i <= 9 ; i++)
 {
-putchar('0'+i);
-if( i < 9 )
+putchar('0' + i);
+if (i < 9)
 {
 putchar(',');
 putchar(' ');
 }
 }
-putchar('$');
 return (0);
 }
