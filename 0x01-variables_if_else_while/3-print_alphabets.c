5162832337bca027ad3d29731df7e8d24a958879
mohamed <malaga.142014@gmail.com>
2023-03-20 12:19:36 -0700

    done

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -6,10 +6,10 @@
  */
 int main(void)
 {
-  char i;
-for ( i = 'a' ; i <= 'z' ; i++)
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
 putchar(i);
-for ( i = 'A' ; i <= 'Z' ; i++)
+for (i = 'A' ; i <= 'Z' ; i++)
 putchar(i);
 putchar('\n');
 return (0);
