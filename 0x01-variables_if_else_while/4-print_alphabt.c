79afd0f6064b29ce1cbd606d38ffd29da0b9bc7d
mohamed <malaga.142014@gmail.com>
2023-03-20 12:47:23 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i < 'e' ; i++)
+putchar(i);
+for (i = 'f' ; i < 'q' ; i++)
+putchar(i);
+for (i = 'r' ; i <= 'z' ; i++)
+putchar(i);
+putchar('\n');
+return (0);
+}
