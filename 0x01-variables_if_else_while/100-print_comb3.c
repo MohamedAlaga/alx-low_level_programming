932fe6eaf55424beddde08c278081d9ab3178cd5
mohamed <malaga.142014@gmail.com>
2023-03-20 17:50:24 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i,y;
+for (i = 0 ; i <= 9 ; i++)
+for (y = 0 ; y <= 9 ; y++)
+{if (y > i){
+putchar('0' + i);
+putchar('0' + y);
+if (i < 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
