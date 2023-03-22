c155090a15d7b748e429b148a578d897e607fd46
mohamed <malaga.142014@gmail.com>
2023-03-22 16:42:47 -0700

    done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+/** 
+* print_alphabet -alphabet 
+*
+*/
+void print_alphabet_x10(void)
+{
+char a ;
+int i;
+for (i = 0 ; i < 10 ; i++){
+for (a = 'a' ; a <= 'z' ; a++)
+_putchar(a);
+_putchar('\n');}
+}
