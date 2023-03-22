d360936775fb986cebeed511fc7560eb79d2dffd
mohamed <malaga.142014@gmail.com>
2023-03-22 16:56:48 -0700

    done

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+/**
+ * _islower - check if lower case 
+ *
+ * Return: one if lower zero if upper (Success)
+ */
+int _islower(int c)
+{
+if (c > 96 && c < 123)
+{
+return (1);
+}
+else 
+{
+return (0);
+}
+}
