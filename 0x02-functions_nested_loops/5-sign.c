61a8f494168201794774c357ef67dce00d258589
mohamed <malaga.142014@gmail.com>
2023-03-22 17:29:01 -0700

    done

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,22 +1,24 @@
-#include <stdio.h>
 /**
- * print_sign - check if lower case
- *@c : is the number th user wants to check
- *
- * Return: one if lower zero if upper (Success)
+ * print_sign - prints the sign of a number
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
  */
-int print_sign(int c)
+int print_sign(int n)
 {
-if (c > 0)
+if (n > 0)
 {
-return ('+1');
+_putchar('+');
+return (1);
 }
-else if (c < 0)
+else if (n == 0)
 {
-return('-1');
-}
-else
+_putchar(48);
+return (0);
+} else if (n < 0)
 {
-return ('00');
+_putchar('-');
 }
+return (-1);
 }
