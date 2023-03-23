f9bec3eeaf0ce194d7d41b849b9af6c0ab77b3c7
mohamed <malaga.142014@gmail.com>
2023-03-22 17:09:03 -0700

    done

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+/**
+ * print_sign - check if lower case
+ *@c : is the number th user wants to check
+ *
+ * Return: one if lower zero if upper (Success)
+ */
+int print_sign(int c)
+{
+if (c > 0)
+{
+printf("+");
+return (1);
+}
+else if (c < 0)
+{
+printf("-");
+return(-1);
+}
+else
+{
+printf("0");
+return (0);
+}
+}
