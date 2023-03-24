17bc1bc65c82c0b313fc00e41117ea0b8a979499
mohamed <malaga.142014@gmail.com>
2023-03-24 15:09:05 -0700

    done

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+* _isupper - check if upper case
+* @c: letter ascii code
+* Return: sum of a and b
+*/
+int _isupper(int c)
+{
+if(c > 64 && c < 91)
+return (1);
+else
+return(0);
+}
