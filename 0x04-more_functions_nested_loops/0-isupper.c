3bc3e256d8e8e4d478b4cf10f3124a2097f2d138
mohamed <malaga.142014@gmail.com>
2023-03-24 16:20:27 -0700

    .

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
-* _isupper - check if upper case
-* @c: letter ascii code
-* Return: sum of a and b
+* _isupper - checks for uppercase character
+* @c: Variable text
+* Return: Always 0.
 */
 int _isupper(int c)
 {
-if (c > 64 && c < 91)
+if (c >= 'A' && c <= 'Z')
+{
 return (1);
+}
 else
+{
 return (0);
+}
 }
