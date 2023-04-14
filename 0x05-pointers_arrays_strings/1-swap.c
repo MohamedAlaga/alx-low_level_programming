86d65fbd5b0c8fdf8fb2a5c83a6f0021ed31c785
mohamed <malaga.142014@gmail.com>
2023-04-14 02:04:05 -0700

    '

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int - swaps the values of two integers.
+ *@a : first user input
+ *@b : second user input
+ *@c :variable used to change variables values
+ *
+ * Return: no return
+ */
+void swap_int(int *a, int *b)
+{
+int *c ;
+c = a;
+a = b;
+b = a;
+}
