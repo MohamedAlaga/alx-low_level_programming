a5414439812241dca7efa9e5c07b2dbe7bb84208
mohamedalaga <malaga.142014@gmail.com>
2023-04-17 14:02:22 +0200

    .

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+/**
+ * _puts - function that prints a string, followed by a new line
+ *@s : user input
+ *
+ * Return: no any
+ */
+void _puts(char *str)
+{
+char *ptrVal = str ;
+do{putchar(*ptrVal);}
+while(*ptrVal++);
+}
