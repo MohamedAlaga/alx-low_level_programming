b0bb73c4c1e0bd23de70a8b9e5991fd39a40edf7
mohamedalaga <malaga.142014@gmail.com>
2023-04-18 14:39:07 +0200

    .

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+* _print_all- print string backword
+*@s : user input
+*
+* Return: no return.
+*/
+void _print_all(char *s)
+{
+if(*s != *"\n")
+{
+_putchar(*s);
+s--;
+_print_all(s);
+}
+else
+{
+_putchar('\n');
+}
+}
+/**
+* _print_rev_recursion - function send the cursour to the end of the string
+*@s : user input
+*
+* Return: no return.
+*/
+\
+void _print_rev_recursion(char *s)
+{
+if (*s != '\0')
+{
+s++;
+_print_rev_recursion(s);
+}
+else
+{
+_print_all(s);
+}
+}
