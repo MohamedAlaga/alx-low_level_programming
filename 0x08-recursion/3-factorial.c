6e5dd904cf4e96eb412dde5d16cda070473d1780
mohamedalaga <malaga.142014@gmail.com>
2023-04-18 15:19:37 +0200

    .

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,9 +1,19 @@
+#include "main.h"
+/**
+*factorial - gets the factorial of aa number
+*@s : user input
+*
+* Return: factorial.
+*/
 int factorial(int n) 
 {
-if(n <= 1)
+if (n = 1)
 return (1);
 else
+if (n < 1)
+return (-1);
+else
 {
-return (n * factorial(n-1));
+return (n * factorial(n - 1));
 }
 }
