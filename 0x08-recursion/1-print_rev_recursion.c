c07baab21f32eb19922ac58991216a7626b721a1
mohamedalaga <malaga.142014@gmail.com>
2023-04-18 14:55:15 +0200

    .

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,39 +1,14 @@
 #include "main.h"
-#include <stdio.h>
 /**
-* _print_all- print string backword
-*@s : user input
-*
-* Return: no return.
-*/
-void _print_all(char *s)
-{
-if (*s != *"\n")
-{
-putchar(*s);
-s--;
-_print_all(s);
-}
-else
-{
-putchar('\n');
-}
-}
-/**
-* _print_rev_recursion - function send the cursour to the end of the string
-*@s : user input
-*
-* Return: no return.
-*/
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
+ */
 void _print_rev_recursion(char *s)
 {
-if (*s != '\0')
+if (*s)
 {
-s++;
-_print_rev_recursion(s);
-}
-else
-{
-_print_all(s);
+_print_rev_recursion(s + 1);
+_putchar(*s);
 }
 }
+
