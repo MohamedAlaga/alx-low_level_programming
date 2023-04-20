59fff4356df3c24acec8e5ed3bf907f18a11fe10
mohamedalaga <malaga.142014@gmail.com>
2023-04-20 14:58:54 +0200

    .

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,17 @@
+/**
+ * _abs - check if lower case
+ *@c : is the number th user wants to check
+ *
+ * Return: one if lower zero if upper (Success)
+ */
+int _abs(int c)
+{
+if (c > 0)
+{
+return (c);
+}
+else
+{
+return (c * -1);
+}
+}
