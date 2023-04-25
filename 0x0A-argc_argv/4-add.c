70924a677ed1d1a7fc62eb1829d71fff043f3276
mohamedalaga <malaga.142014@gmail.com>
2023-04-26 00:13:05 +0200

    .

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print args in new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{int x, y, i;
+if (argc == 1)
+{
+printf("0\n");
+return 0;
+}
+else if (argc > 1)
+{
+x = 0 ;
+for(i = 1; i < argc; i++)
+{
+if (*argv[i]>= 48 && *argv[i] <= 57)
+{
+y = atoi(argv[i]);
+x = x+y;
+}
+else
+{
+printf("Error\n");
+return (0);
+}
+}
+}
+printf("%d\n",x);
+return (0);
+}
