3ae124b5ad39b2eb5f8eefd4e5b692ee1305dd9e
mohamedalaga <malaga.142014@gmail.com>
2023-04-25 22:12:31 +0200

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,11 +1,13 @@
 #include "stdio.h"
+
  /**
  * main : starting point
- * argc : int
- * argv : str
+ * @argc : int
+ * @argv : str
  * Return: 0
  */
-int main(int argc,char *argv[])
+
+int main(int argc, char *argv[])
 {
 (void)argc;
 printf("%s\n", argv[0]);
