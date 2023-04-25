9431a254c46ad858a288102ca615734b486e7940
mohamedalaga <malaga.142014@gmail.com>
2023-04-25 22:03:39 +0200

    .

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,12 @@
 #include "main.h"
+ /**
+ * main : starting point
+ *
+ * Return: none
+ */
 int main(int argc,char *argv[])
 {
-    (void)argc;
-_putchar(**argv);
+(void)argc;
+_putchar(argv[0]);
 return (0) ;
 }
