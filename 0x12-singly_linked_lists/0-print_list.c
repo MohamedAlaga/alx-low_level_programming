01561b9ca8dd1df564146ce27b7d131ae019bf84
mohamedalaga <malaga.142014@gmail.com>
2023-04-28 03:47:41 +0300

    .

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include"main.h"
+#include<stdio.h>
+#include<string.h>
+/**
+ * print_list - prints all the elements of a list
+ * @h:linked list
+ * Return: 0
+ */
+size_t print_list(const list_t *h)
+{if(h->str == NULL)
+{
+printf("[0] (nil)");
+return (0);
+}
+else
+{
+printf("%s",h->str);
+}
+return (1 + print_list(h->next));
+}
