5c3659a92bab84e38179ea3e9a15153cacb03c9f
mohamedalaga <malaga.142014@gmail.com>
2023-04-28 08:58:26 +0300

    .

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,7 +16,23 @@ return (NULL);
 }
 else if (s2 == NULL)
 {
-return (s1);
+char *concat = NULL;
+int len1;
+len1 = strlen(s1);
+concat = malloc((len1 + 1) * sizeof(char));
+if (concat == NULL)
+{
+return (NULL);
+}
+else
+{
+int i;
+for(i = 0; i < (len1); i++)
+{
+concat[i] = s1[i];
+}
+return (concat);
+}
 }
 else if (s1 == NULL)
 {
