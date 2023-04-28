b40b582050962ec69ff51db41741adc53dd14e47
mohamedalaga <malaga.142014@gmail.com>
2023-04-28 08:06:58 +0300

    .

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,51 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+/**
+ * _strdup - creates an array of chars
+ * @str: array of chars
+ * Return: 0
+ */
+char *str_concat(char *s1, char *s2)
+{
+if (s1 == NULL)
+{
+if (s2 == NULL)
+{
+return (NULL);
+}
+return (s2);
+}
+else if (s2 == NULL)
+{
+return (s1);
+}
+else
+{
+char *concat = NULL;
+int len1,len2;
+len1 = strlen(s1);
+len2 = strlen(s2);
+concat = malloc((len1+len2)*sizeof(char));
+if (concat == NULL)
+{
+return (NULL);
+}
+else
+{
+int i;
+for(i = 0; i<(len1+len2);i++)
+{
+if(i < len1)
+{
+concat[i] = s1[i];
+}
+else
+{
+concat[i] = s2[i-len1]; 
+}
+return (concat);
+}
+}
+}
+}
