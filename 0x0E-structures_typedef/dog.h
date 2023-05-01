ee084ecf12d8c6c91e083874e65a62151dd84f0a
mohamedalaga <malaga.142014@gmail.com>
2023-05-01 11:12:09 +0300

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,13 @@
 #ifndef dog_h
 #define dog_h
+/**
+ * struct dog - Define a new type struct dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner 
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 char *name;
