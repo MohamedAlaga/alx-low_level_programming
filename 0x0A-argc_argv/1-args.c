bead880c033326a6bbe65a083d3f3af61960b90c
MohamedAlaga <malaga.142014@gmail.com>
2023-09-02 05:41:25 +0300

    z

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - print arg number
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{(void)argv;
+printf("%d\n", argc);
+return (0);
+}
