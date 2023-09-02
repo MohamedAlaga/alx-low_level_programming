7e30cada4b7d7350574d1706ff81bc17dbfec0b2
MohamedAlaga <malaga.142014@gmail.com>
2023-09-02 05:48:45 +0300

    zz

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - print args in new line
+ * @argc: int
+ * @argv: list
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+if (argc == 3)
+{
+int multi, x, y;
+x = atoi(argv[1]);
+y = atoi(argv[2]);
+multi = x *y;
+printf("%d\n", multi);
+return (0);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+}
