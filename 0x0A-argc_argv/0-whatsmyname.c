86898f8772c5f4721108afd291d97791f1976a01
MohamedAlaga <malaga.142014@gmail.com>
2023-09-01 19:34:49 -0700

    '

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,9 +9,5 @@ int main(int argc, char *argv[])
 <<<<<<< HEAD
 {(void)argc;
 printf("%s\n",argv[0]);
-=======
-{   (void)argc;
-printf("%s\n", argv[0]);
->>>>>>> bdff54f59c6604f6287283a9a6e2629fb5fa9740
 return (0);
 }
