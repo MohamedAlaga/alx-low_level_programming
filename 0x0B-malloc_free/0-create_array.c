a695c4e5eee3c6fa83c295e017d32ae83018a312
MohamedAlaga <malaga.142014@gmail.com>
2023-09-06 05:24:36 +0300

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
 char *create_array(unsigned int size, char c)
 {
 char *str = malloc(sizeof(char)*size);int i ;
-if (size == 0){return NULL;}
+if (size == 0 || str == NULL){return NULL;}
 for(i = 0; i<(int)size ; i++)
 {
 str[i]=c;
