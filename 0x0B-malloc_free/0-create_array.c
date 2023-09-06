289581abc1587bb88391217925b2b302650e621a
MohamedAlaga <malaga.142014@gmail.com>
2023-09-06 05:30:20 +0300

    .

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,11 +3,13 @@
 #include<stdio.h>
 char *create_array(unsigned int size, char c)
 {
-char *str = malloc(sizeof(char)*size);int i ;
-if (size == 0 || str == NULL){return NULL;}
-for(i = 0; i<(int)size ; i++)
+char *str = malloc(sizeof(char) * size);
+int i;
+if (size == 0 || str == NULL)
+return NULL;
+for (i = 0; i < (int)size ; i++)
 {
-str[i]=c;
+str[i] = c;
 }
-return str;
+return (str);
 }
