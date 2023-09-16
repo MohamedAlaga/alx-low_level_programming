1ea6f83a06fc2f481cb059e0f39234b89ba705b6
MohamedAlaga <malaga.142014@gmail.com>
2023-09-16 04:35:21 +0300

    done

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * sum_them_all - return arg sum
+ * @n: int
+ * Return: sum of all args
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+unsigned int i = 0;
+va_list strs;
+va_start(strs, n);
+for (i = 0; i < n; i++)
+{
+char *str1 =  va_arg(strs, char*);
+if (str1 != NULL)
+printf("%s",str1);
+else 
+printf("nil");
+if (i != n-1 )
+printf("%s", separator);
+}
+printf("\n");
+}
