13dcf2f790f12c6a552dda12ce95e482a118186e
MohamedAlaga <malaga.142014@gmail.com>
2023-09-16 05:41:05 +0300

    done

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,8 +1,9 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
- * sum_them_all - return arg sum
- * @n: int
+ * print_strings - print strings in arg
+ * @separator : string
+ * @n : int
  * Return: sum of all args
  */
 void print_strings(const char *separator, const unsigned int n, ...)
@@ -14,10 +15,10 @@ for (i = 0; i < n; i++)
 {
 char *str1 =  va_arg(strs, char*);
 if (str1[0] != '\0')
-printf("%s",str1);
-else 
+printf("%s", str1);
+else
 printf("nil");
-if (i != n-1 )
+if (i != n - 1)
 printf("%s", separator);
 }
 printf("\n");
