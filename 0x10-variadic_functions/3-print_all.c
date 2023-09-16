4a8a376f2801cc3acb746c322336b02eb15aca7f
MohamedAlaga <malaga.142014@gmail.com>
2023-09-16 05:28:57 +0300

    done

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -5,7 +5,8 @@
  * print_all - prints anything
  * @format: list of types of arguments passed to the function
  */
-void print_all(const char *const format, ...) {
+void print_all(const char *const format, ...)
+{
 int i = 0;
 char *str, *sep = "";
 va_list list;
