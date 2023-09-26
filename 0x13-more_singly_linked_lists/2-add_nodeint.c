2694c837cef09f4bff0c580e5d42e186d4826844
MohamedAlaga <malaga.142014@gmail.com>
2023-09-27 01:49:27 +0300

    done

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -2,7 +2,6 @@
 /**
  * add_nodeint - add node to the list
  * @head: pointer to list last element
-* @n: intger
  *
  * Description: return singly linked list node structure len
  * Return: lenght of list
@@ -12,6 +11,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 if (head && n)
 {
 listint_t *h;
+h = *head;
 h->n = n;
 h->next = *head;
 return (h);
