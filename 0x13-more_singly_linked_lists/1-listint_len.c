d5d5d2564cd113cc6379d6315ec6f5233bab680c
MohamedAlaga <malaga.142014@gmail.com>
2023-09-27 01:03:00 +0300

    done

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_listint - singly linked list
+ * @h: linked list
+ *
+ * Description: singly linked list node structure
+ * Return: lenght of list
+ */
+size_t print_listint(const listint_t *h)
+{
+if(h)
+{
+if (h->next == NULL)
+return (1);
+else
+return (1 + print_listint(h->next)); }
+return (0);
+}
