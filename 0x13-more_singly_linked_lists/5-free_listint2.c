3cab5e7e87b16fb39c30015b6dd60408c473692f
MohamedAlaga <malaga.142014@gmail.com>
2023-09-27 03:33:58 +0300

    done

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -6,6 +6,8 @@
  */
 void free_listint2(listint_t **head)
 {
+if (*head)
+{
 listint_t *h = *head;
 if (!h)
 free(h);
@@ -13,4 +15,5 @@ else
 free_listint2(&(h->next));
 free(h);
 *head = NULL;
+}
 }
