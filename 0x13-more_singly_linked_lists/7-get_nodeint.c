edfded4e7d94240e1375c081e105dc335bc9b7b9
MohamedAlaga <malaga.142014@gmail.com>
2023-09-27 04:42:01 +0300

    done

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * pop_listint - delete head node
+ * @head: pointer to the first node in the list
+ *
+ * Return:head node data or 0 if
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *temp = head;
+int count;
+for (count = 0; count != index; count++)
+{
+temp = temp->next;
+}
+return (temp);
+}
