e5dffc0d32a36816fe531307534ad23e8917cc65
MohamedAlaga <malaga.142014@gmail.com>
2023-10-02 13:02:04 -0700

    .

---
+++ 0x15-file_io/0-read_textfile .c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * read_textfile - print file
+ *@filename : file
+ *@letters : number of printed cahrs
+ * Return: number of charecters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters) {
+FILE *f = fopen(filename, "r");
+char x;
+int count = 0;
+if ((filename == NULL) || letters == 0)
+return (0);
+while (fscanf(f, "%c", &x) == 1 && count < (int)letters) {
+printf("%c", x);
+count++;
+}
+return (count);
+}
