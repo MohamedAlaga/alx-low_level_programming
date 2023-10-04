8986698c75c88685efdecc39471018f8bfc0d3a2
MohamedAlaga <malaga.142014@gmail.com>
2023-10-04 13:57:39 -0700

    second task

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+int create_file(const char *filename, char *text_content)
+{
+int f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600), len;
+if (filename == NULL || f == -1)
+return -1;
+if (text_content == NULL)
+{
+text_content = "";
+}
+for (len = 0; text_content[len] != '\0'; len++)
+continue;
+write(f, text_content, len);
+close(f);
+return 1;
+}
