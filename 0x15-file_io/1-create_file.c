06f76fdcd780d5a8e4bff59ebb8767d078572c35
MohamedAlaga <malaga.142014@gmail.com>
2023-10-04 14:01:10 -0700

    second task betty doc

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,10 @@
 #include "main.h"
-
+/**
+ * create_file - print file
+ *@filename : file directory
+ *@text_content : string
+ * Return: 1 on success -1 o fail
+ */
 int create_file(const char *filename, char *text_content)
 {
 int f = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600), len;
