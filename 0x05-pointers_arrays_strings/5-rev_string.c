5a4877463d26757cbe7facf4a16c3a4778fc840f
MohamedAlaga <malaga.142014@gmail.com>
2024-06-18 14:25:03 +0300

    puts2

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 /**
- * rev_string - function that reverse string.
- * @s : string to  reverse
+ * puts2 - function that prints every other character of a string.
+ * @str : string to  print
  *
  * Return: no any
  */
-void rev_string(char *s)
+void puts2(char *str)
 {
-int i;
-char temp;
 int len = 0;
-while (s[len] != '\0') {
+while (str[len] != '\0') {
 len++;
 }
-for (i = 0; i < len / 2; i++) {
-temp = s[i];
-s[i] = s[len - i - 1];
-s[len - i - 1] = temp;
+for (int i = 0; i < len ; i++) {
+if(i%2 == 0)
+{
+printf("%c", str[i]);
 }
+}
+printf("\n");
 }
