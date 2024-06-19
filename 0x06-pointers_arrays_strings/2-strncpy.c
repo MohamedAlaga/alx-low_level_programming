b3d0440742fb5a79d2c16677f819f46313d2ebe8
MohamedAlaga <malaga.142014@gmail.com>
2024-06-19 14:31:58 +0300

    _strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * _strncpy -  copy the src string to the dest string
+ * @dest: destination strig
+ * @src: source strnig
+ * @n: number of bytes to append
+ *
+ * Return: the int converted from the string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+  int i;
+
+  i = 0;
+
+  while (src[i] != '\0' && i < n)
+  {
+    dest[i] = src[i];
+    i++;
+  }
+
+  while (i < n)
+  {
+    dest[i] = '\0';
+    i++;
+  }
+
+  return (dest);
+}
