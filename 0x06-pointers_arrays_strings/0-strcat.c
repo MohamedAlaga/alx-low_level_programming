4669331dc09aae723ff2718f5eea56b659541f63
MohamedAlaga <malaga.142014@gmail.com>
2024-06-19 14:05:17 +0300

    _strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * _strcat -  appends the src string to the dest string
+ * @dest: destination strig
+ * @src: source strnig
+ *
+ * Return: the int converted from the string
+ */
+char *_strcat(char *dest, char *src)
+{
+  int i, len = 0, len2 = 0;
+  for (i = 0; dest[i] != '\0'; i++)
+  {
+    len++;
+  }
+  for (i = 0; src[i] != '\0'; i++)
+  {
+    len2++;
+  }
+  for (i = 0; i < len2; i++)
+  {
+    dest[len + i] = src[i];
+  }
+  dest[len + len2] = '\0';
+  return (dest);
+}
