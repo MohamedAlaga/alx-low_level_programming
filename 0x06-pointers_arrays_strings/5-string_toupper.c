9ce2ce2312461c81c27572643048760ad53e43cb
MohamedAlaga <malaga.142014@gmail.com>
2024-06-23 20:02:12 +0300

    string_toupper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * string_toupper -  convert any letter to Upper case
+ * @arr: array of string
+ *
+ * Return: the upper string
+ */
+char *string_toupper(char *arr)
+{
+  int len, i;
+  len = strlen(arr);
+  for (i = 0; i < len; i++)
+    if (arr[i] >= 'a' && arr[i] <= 'z')
+      arr[i] = arr[i] - 32;
+  return arr;
+}
