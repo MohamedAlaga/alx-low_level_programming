8f8857dbb7769dcea36b37fb86c4ff067928e1d4
MohamedAlaga <malaga.142014@gmail.com>
2024-06-23 20:16:44 +0300

    cap_string

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * string_toupper -  convert any letter to Upper case
+ * cap_string -  capitalizes all words of a string.
  * @arr: array of string
  *
- * Return: the upper string
+ * Return: the capitalized string
  */
-char *string_toupper(char *arr)
+char *cap_string(char *arr)
 {
-  int len, i;
-  len = strlen(arr);
-  for (i = 0; i < len; i++)
-    if (arr[i] >= 'a' && arr[i] <= 'z')
-      arr[i] = arr[i] - 32;
-  return arr;
+    int i,len;
+    len = strlen(arr);
+    for(i=0 ; i<len ; i++)
+        if(arr[i] == '\n' || arr[i] == '\t' || arr[i] == ' ' || arr[i] == ',' || arr[i] == ';' || arr[i] == '.' || arr[i] == '!' || arr[i] == '?' || arr[i] == '\"' || arr[i] == '(' || arr[i] == ')' || arr[i] == '{' ||arr[i] == '}' )
+        {
+        if(arr[i+1] >= 'a' && arr[i+1] <= 'z')
+            arr[i+1] = arr[i+1] - 32;
+        }
+    return (arr);
 }
