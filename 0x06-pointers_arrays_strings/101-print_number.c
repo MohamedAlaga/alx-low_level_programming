da262930b72f6f40a5981df8d471ba446c577ffb
MohamedAlaga <malaga.142014@gmail.com>
2024-06-23 20:38:18 +0300

    print_number fix

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+
 /**
   * print_number - print numbers chars
   * @n: integer params
@@ -13,7 +14,7 @@ void print_number(int n)
 
 	if (n < 0)
 	{
-		putchar('-');
+		_putchar('-');
 		n1 = -n;
 	}
 
@@ -21,5 +22,5 @@ void print_number(int n)
 	{
 		print_number(n1 / 10);
 	}
-	putchar((n1 % 10) + '0');
+	_putchar((n1 % 10) + '0');
 }
