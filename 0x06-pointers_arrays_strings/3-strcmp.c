3c038cc6379080a4eac7fe74b75d31d57f1e8fc7
MohamedAlaga <malaga.142014@gmail.com>
2024-06-23 13:32:52 +0300

    _strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp -  compare two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: deferance of the first unmatched character
+ */
+int _strcmp(char *s1, char *s2) {
+  int i, len1, len2;
+  len1 = strlen(s1);
+  len2 = strlen(s2);
+  if (len2 > len1)
+    len1 = len2;
+  for (i = 0; i < len1; i++)
+    if (s1[i] - s2[i] != 0)
+      return s1[i] - s2[i];
+  return 0;
+}
