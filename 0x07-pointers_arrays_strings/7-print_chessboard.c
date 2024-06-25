a9f34c274733edc5df38c15044f28d908ca990d2
MohamedAlaga <malaga.142014@gmail.com>
2024-06-25 12:25:38 +0300

    _strstr

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * print_chessboard - function that prints the chessboard.
+ * @a:matrix for the chessboard .
+ *
+ * Return: none.
+ */
+void print_chessboard(char (*a)[8])
+{
+int i, j;
+
+for (i = 0; a[i][0] != '\0'; i++)
+{
+for (j = 0; j < 8; j++)
+printf("%c", a[i][j]);
+printf("\n");
+}
+}
