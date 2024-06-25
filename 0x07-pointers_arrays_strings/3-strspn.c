d6c5abbfdf4b58f9acea4d741aa3c60691eaa59f
MohamedAlaga <malaga.142014@gmail.com>
2024-06-25 10:52:06 +0300

    _strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+/**
+ * _strspn - gets the length of a prefix substring.
+ * @s: string to search inside .
+ * @accept: bytes to accept.
+ *
+ * Return: pointer to the string.
+ */
+unsigned int _strspn(char *s, char *accept) {
+  unsigned int num, sum, i, j;
+
+  sum = 0;
+  for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
+    for (j = 0; accept[j] != '\0'; j++)
+      if (s[i] == accept[j]) {
+        sum++;
+        break;
+      }
+  return (sum);
+}
