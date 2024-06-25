3afe54fd16cb363b6ebfde43b35bc70ec504f3ba
MohamedAlaga <malaga.142014@gmail.com>
2024-06-25 11:22:38 +0300

    _strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,12 +9,11 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-unsigned int sum, i, j;
-sum = 0;
-for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
-for (j = 0; accept[j] != '\0'; j++)
-if (s[i] == accept[j]) {
-return (&s[i]);
-}
-return (NULL);
+  unsigned int i, j;
+  for (i = 0; s[i] != '\0' && s[i] != ' '; i++)
+    for (j = 0; accept[j] != '\0'; j++)
+      if (s[i] == accept[j]) {
+        return (&s[i]);
+      }
+  return (NULL);
 }
