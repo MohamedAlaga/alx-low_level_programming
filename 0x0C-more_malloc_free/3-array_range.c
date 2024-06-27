1e177a248dce1176318348ad0d2b1b9bf7540576
MohamedAlaga <malaga.142014@gmail.com>
2024-06-27 12:27:25 +0300

    adv

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -9,14 +9,16 @@
  *
  * Return:  pointer to the allocated memory.
  */
-int *array_range(int min, int max) {
-  int i, *ptr;
-  if (min > max)
-    return (NULL);
-  ptr = malloc(sizeof(int) * (max - min + 1));
-  if (ptr == NULL)
-    return (NULL);
-  for (i = 0; i < max - min + 1; i++)
-    ptr[i] = min + i;
-  return ptr;
+int *array_range(int min, int max)
+{
+int i, *ptr;
+
+if (min > max)
+return (NULL);
+ptr = malloc(sizeof(int) * (max - min + 1));
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < max - min + 1; i++)
+ptr[i] = min + i;
+return ptr;
 }
