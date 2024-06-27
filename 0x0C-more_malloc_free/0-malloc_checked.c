c4cbdee78b80851e69700f2494e59a9b74396e40
MohamedAlaga <malaga.142014@gmail.com>
2024-06-27 11:43:19 +0300

    string_nconcat

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -7,15 +7,16 @@
  *
  * Return:  pointer to the allocated memory.
  */
-void *malloc_checked(unsigned int b) {
-  void *ptr;
-  ptr = malloc(b);
-  if(ptr == NULL)
-  {
-    exit(98);
-  }
-  else
-  {
-    return (ptr);
-  }
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+} else
+{
+return (ptr);
+}
 }
---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * malloc_checked - function that allocates memory using malloc.
+ * @b:size to be allocated .
+ *
+ * Return:  pointer to the allocated memory.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+int len1, len2, i;
+char *ptr;
+  
+len1 = strlen(s1);
+len2 = strlen(s2);
+if (len2 == 0)
+return s1;
+if (n < len2)
+len2 = n;
+ptr = malloc(len1 + len2);
+if (ptr == NULL)
+return (NULL);
+for (i = 0; i < len1; i++)
+ptr[i] = s1[i];
+for (; i < len1 + len2; i++)
+ptr[i] = s2[i - len1];
+return ptr;
+}
