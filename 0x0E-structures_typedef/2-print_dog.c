cc23154be8b0904e747c23e0cb7e8193ab5e2785
MohamedAlaga <malaga.142014@gmail.com>
2024-07-08 11:04:13 +0300

    print_dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,7 +7,9 @@
 
 void print_dog(struct dog *d)
 {
+if (d == NULL)
+return;
 printf("Name: %s\n", d->name == NULL ? "(nil)" : d->name);
-printf(d->age <= 0 ? "Age: (nil)\n" : "Age: %f\n", d->age);
+printf("Age: %f\n", d->age);
 printf("Owner: %s\n", d->owner == NULL ? "(nil)" : d->owner);
 }
