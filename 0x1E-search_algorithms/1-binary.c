4c6d53f66fe2bd2834e7ee5f4e276d65f09dd360
MohamedAlaga <malaga.142014@gmail.com>
2024-07-16 13:25:13 +0300

    linear_search

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,7 +9,7 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-size_t low = 0, heigh = size - 1, mid = (low + heigh) / 2, i, y;
+size_t low = 0, heigh = size - 1, mid = (low + heigh) / 2, y;
 while (low <= heigh)
 {
 printf("Searching in array:");
