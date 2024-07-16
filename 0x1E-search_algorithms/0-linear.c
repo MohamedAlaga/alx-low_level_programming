276577b31f2ef72934b1d821da5361cb1e7abcc7
MohamedAlaga <malaga.142014@gmail.com>
2024-07-16 12:51:39 +0300

    linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,12 @@
 #include "search_algos.h"
-
+/**
+ * linear_search - performe a linear search on array
+ * @array: array to search inside
+ * @size: size of the array
+ * @value: value to search for in the array
+ * 
+ * Return: Always EXIT_SUCCESS
+ */
 int linear_search(int *array, size_t size, int value)
 {
 size_t i;
