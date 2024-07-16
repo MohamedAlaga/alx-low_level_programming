fc9d3a3831290b74d0c0b31c5b5113b66dd6d2a2
MohamedAlaga <malaga.142014@gmail.com>
2024-07-16 12:43:57 +0300

    linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,10 +5,10 @@ int linear_search(int *array, size_t size, int value)
 size_t i;
 for (i = 0; i < size; i++)
 {
-printf("Value checked array[%zu] = [%d]", i, array[i]);
+printf("Value checked array[%d] = [%d]", i, array[i]);
 if (array[i] == value)
 {
-printf("Found %d at index: [%zu]\n", value, i);
+printf("Found %d at index: [%d]\n", value, i);
 return (i);
 }
 }
