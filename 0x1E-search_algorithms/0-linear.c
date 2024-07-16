baea9fe904c9bae59f9a309629054d455d1887f1
MohamedAlaga <malaga.142014@gmail.com>
2024-07-16 12:52:43 +0300

    linear_search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -4,7 +4,7 @@
  * @array: array to search inside
  * @size: size of the array
  * @value: value to search for in the array
- * 
+ *
  * Return: Always EXIT_SUCCESS
  */
 int linear_search(int *array, size_t size, int value)
