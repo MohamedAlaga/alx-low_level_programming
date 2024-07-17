956af4f3552b3d9cdd98b174d9448381be72898a
MohamedAlaga <malaga.142014@gmail.com>
2024-07-17 19:00:09 +0300

    exponential_search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,5 +1,44 @@
 #include "search_algos.h"
 /**
+ * binary_search - performe a binary search on array
+ * @array: array to search inside
+ * @size: size of the array
+ * @value: value to search for in the array
+ *
+ * Return: -1 if not found or the index of the first appear of value
+ */
+int binary_search(int *array, size_t size, int value)
+{
+size_t low = 0, heigh = size - 1, mid = (low + heigh) / 2, y;
+if (array == NULL)
+return (-1);
+while (low <= heigh)
+{
+printf("Searching in array:");
+for (y = low; y <= heigh; y++)
+{
+printf(" %d", array[y]);
+if (y != heigh)
+printf(",");
+}
+printf("\n");
+if (array[mid] > value)
+{
+heigh = mid - 1;
+mid = (low + heigh) / 2;
+}
+else if (array[mid] < value)
+{
+low = mid + 1;
+mid = (low + heigh) / 2;
+}
+else if (array[mid] == value)
+return (mid);
+}
+return (-1);
+}
+
+/**
  * exponential_search - performe a exponential search on array
  * @array: array to search inside
  * @size: size of the array
