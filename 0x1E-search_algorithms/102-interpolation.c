a7aaa97f55d73eb59aa06a4d1b157f9ab6b43782
MohamedAlaga <malaga.142014@gmail.com>
2024-07-17 13:20:11 +0300

    interpolation_search

---
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+/**
+ * interpolation_search - performe a jump search on array
+ * @array: array to search inside
+ * @size: size of the array
+ * @value: value to search for in the array
+ *
+ * Return: -1 if not found or the index of the first appear of value
+ */
+int interpolation_search(int *array, size_t size, int value)
+{
+int *temp = array, val;
+static size_t posStat = 0;
+size_t low = 0, heigh = size - 1,
+pos = (low + (value - temp[low]) * (heigh - low) /
+                          (temp[heigh] - temp[low]));
+posStat += pos;
+if (array == NULL || size == 0)
+return (-1);
+if (pos > size || pos < 0)
+{
+printf("Value checked array[%lu] is out of range\n", pos);
+return (-1);
+}
+printf("Value checked array[%lu] = [%d]\n", posStat, temp[posStat]);
+if (value < temp[pos])
+{
+return (pos - interpolation_search(temp, (size - pos), value));
+}
+else if (value > temp[pos])
+{
+return (pos + interpolation_search(temp + pos, (size - pos), value));
+}
+else if (value == temp[pos])
+return (pos);
+return (-1);
+}
