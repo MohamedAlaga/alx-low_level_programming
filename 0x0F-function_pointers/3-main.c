cc6f9e737b19fbe2524fd95ea07dc34b9a22a6ea
MohamedAlaga <malaga.142014@gmail.com>
2024-07-18 18:08:49 +0300

    function pointers 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,23 @@
+#include "3-calc.h"
+int main(int argc, char *argv[]) {
+  int (*result)(int,int) = get_op_func(argv[2]);
+  if (argc != 4)
+    {
+      printf("Error\n");
+      exit(98);
+    }
+
+    if (argv[2][1])
+    {
+      printf("Error\n");
+      exit(99);
+    }
+  if (result == NULL)
+    {
+      printf("Error\n");
+      exit(99);
+    }
+  (void)argc;
+  printf("%d\n", result(atoi(argv[1]), atoi(argv[3])));
+  return (0);
+}
