26e12a756e5df4d914278a63b78285719a5ca0f5
MohamedAlaga <malaga.142014@gmail.com>
2024-07-18 13:42:08 +0300

    function pointers 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - use a func pointer
+ * @name: pointer to string
+ * @f: ponter to function to use
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+f(name);
+}
