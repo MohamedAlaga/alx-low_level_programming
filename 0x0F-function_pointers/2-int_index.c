cc74529f3e81454b620c39ec7e2d02404787236c
MohamedAlaga <malaga.142014@gmail.com>
2024-07-18 14:17:19 +0300

    function pointers 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+/**
+ * int_index - use a func pointer with all elements of arr
+ * @array: array to use with func
+ * @size: size of the array
+ * @action: function pointer
+ *
+ * Return: index of the first 1 ,on false -1.
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size > 0 && array != NULL)
+for (i = 0; i < size; i++) {
+if (cmp(array[i]) == 1) {
+return (i);
+}
+}
+return (-1);
+}
