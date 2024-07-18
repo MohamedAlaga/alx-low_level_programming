04bdfda0ce1098b7f8211c63fc565e66da13eb99
MohamedAlaga <malaga.142014@gmail.com>
2024-07-18 18:23:55 +0300

    function pointers 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,7 +1,14 @@
 #include "3-calc.h"
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
+ *
+ * Return: Always 0.
+ */
 int main(int argc, char *argv[])
 {
-int (*result)(int,int) = get_op_func(argv[2]);
+int (*result)(int, int) = get_op_func(argv[2]);
 if (argc != 4)
 {
 printf("Error\n");
